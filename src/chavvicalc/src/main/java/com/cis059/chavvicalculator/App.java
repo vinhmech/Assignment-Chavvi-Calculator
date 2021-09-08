700dcd9f8f028a08007cd9b3f96d23d3f6e53ddf
vinhmech <vincenguyen262@gmail.com>
2021-09-07 19:53:07 -0700

    Added hello world with maven project

---
+++ src/chavvicalc/src/main/java/com/cis059/chavvicalculator/App.java
@@ -0,0 +1,13 @@
+package com.cis059.chavvicalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
