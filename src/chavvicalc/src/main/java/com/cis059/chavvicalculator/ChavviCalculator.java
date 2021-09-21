1bbbb9bd5daa61ecc0e51991dfb73d1eb488311b
vinhmech <vincenguyen262@gmail.com>
2021-09-20 19:58:28 -0700

    Added program description

--- src/chavvicalc/src/main/java/com/cis059/chavvicalculator/ChavviCalculator.java
+++ src/chavvicalc/src/main/java/com/cis059/chavvicalculator/ChavviCalculator.java
@@ -1,9 +1,16 @@
+/**
+ *  Copyright Notice
+ *  Filename: ChavviCalculator.java
+ */
+
 package com.cis059.chavvicalculator;
 
-/**
- * Hello world!
- *
+/** 
+ * Create a calculator
+ * @author Vinh Nguyen - SJCC_CIS059
+ * @version 1.0 - 09/20/2021
  */
+
 public class ChavviCalculator 
 {
     public static void main( String[] args )
